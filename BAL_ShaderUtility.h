f451b0aa38fc6c1124f412f23944ee55e8d62d3f
Brandon <brandonreallen@gmail.com>
2018-02-02 22:21:12 -0500

    Add the utility

---
+++ BAL_ShaderUtility.h
@@ -0,0 +1,10 @@
+#ifndef H_BAL_SHADERUTILITY
+#define H_BAL_SHADERUTILITY
+
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+void 
+CreateShaderProgram(GLuint* Program, char* VertexShader, char* FragmentShader);
+
+#endif
